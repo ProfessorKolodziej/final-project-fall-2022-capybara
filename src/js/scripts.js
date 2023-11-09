9c666ca0ff951cdabbc66bd6dda2706c560a6019
rexyzw98 <ziwenye@bu.edu>
2023-11-08 23:52:33 -0500

    start

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,43 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+enter.addEventListener('click', function () {
+	showPage('menuPage');
+});
+
+meditation.addEventListener('click', function () {
+	showPage('meditationPage');
+});
+
+storyTime.addEventListener('click', function () {
+	showPage('storyPage');
+});
+
+backtoHome.addEventListener('click', function () {
+	showPage('homePage');
+});
+
+backtoMenu.addEventListener('click', function () {
+	showPage('menuPage');
+});
+
+startMeditation.addEventListener('click', function () {
+	showPage('meditationStartPage');
+});
+
+backtoMeditation.addEventListener('click', function () {
+	showPage('meditationPage');
+});
+
+//.startStop.addEventListener('click', function () {showPage('meditationPage');});
+
+
+
+function showPage(pageId) {
+	const pages = document.querySelectorAll('.page');
+	pages.forEach(page => {
+		page.style.display = 'none';
+	});
+	document.getElementById(pageId).style.display = 'block';
+}
